f0f81d6248fd8924b8a3376b415e6b8d561225a2
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-23 18:02:07 +0200

    s3 test

--- tf-s3.tf
+++ tf-s3.tf
@@ -31,22 +31,25 @@ resource "aws_s3_bucket_public_access_block" "mmm1example" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+resource "aws_s3_bucket_policy" "mmm1example-policy" {
   bucket = aws_s3_bucket.mmm1example.id
-  policy = <<EOF
- {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicRead",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject",
-                "s3:GetObjectVersion"
-            ],
-            "Resource": "arn:aws:s3:::mmm1-example/*"
-        }
-    ]
- }
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "MYBUCKETPOLICY",
+  "Statement": [
+    {
+      "Sid": "IPAllow",
+      "Effect": "Deny",
+      "Principal": "*",
+      "Action": "s3:*",
+      "Resource": "arn:aws:s3:::mmm1-example/*",
+      "Condition": {
+         "IpAddress": {"aws:SourceIp": "8.8.8.8/32"}
+      }
+    }
+  ]
+}
+POLICY
 }
