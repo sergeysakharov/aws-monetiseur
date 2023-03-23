027aa48c3bde0ad1a83af7e21879602acf849fcc
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-23 11:57:25 +0200

    iam policy

--- tf-iam.tf
+++ tf-iam.tf
@@ -1,3 +1,23 @@
 resource "aws_iam_user" "tf-iam-ses" {
     name = "tf-ses"
 }
+
+resource "aws_iam_user_policy" "tf-iam-ses-policy" {
+#  count = length(var.username)
+  name = "tf-iam-ses-policy"
+#  user = element(var.username,count.index)
+policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
