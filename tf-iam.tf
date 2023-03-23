17b6711a5b301ccbb8dfe1f0db9800af2cc89860
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-23 11:58:11 +0200

    ses policy

--- tf-iam.tf
+++ tf-iam.tf
@@ -5,7 +5,7 @@ resource "aws_iam_user" "tf-iam-ses" {
 resource "aws_iam_user_policy" "tf-iam-ses-policy" {
 #  count = length(var.username)
   name = "tf-iam-ses-policy"
-#  user = element(var.username,count.index)
+  user = tf-ses
 policy = <<EOF
 {
   "Version": "2012-10-17",
