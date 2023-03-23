124d2783b8c4e82ea7699f7d79d3186db859a271
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-23 10:25:25 +0200

    s3

--- tf-s3-env.tf
+++ tf-s3-env.tf
@@ -1,13 +1,19 @@
 #https://registry.terraform.io/modules/terraform-aws-modules/s3-bucket/aws/latest
 
-module "s3_bucket-env" {
+module "s3-bucket-env" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "env"
+  bucket = "s3-${var.project}-env"
   acl    = "private"
 
   versioning = {
     enabled = true
+  }
+
+  tags = {
+    Terraform = "true"
+    Environment = var.envnonprod
+    Project = var.project
   }
 
 }
