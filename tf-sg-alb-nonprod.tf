c4931525e287c5e151a6d4ec78ad2b248c5c152c
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-23 15:01:50 +0200

    alb sg update

--- tf-sg-alb-nonprod.tf
+++ tf-sg-alb-nonprod.tf
@@ -4,25 +4,27 @@ module "tf-sg-alb-nonprod" {
   name        = "tf-sg-alb-nonprod"
   vpc_id      = module.vpc-nonprod.vpc_id
 
-  ingress_cidr_blocks      = ["10.0.0.0/16"]
-  ingress_rules            = ["https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.0.0.0/16"
-    },
-    {
-      rule        = "redis-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      rule        = "smtps-465-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
+  ingress_rules            = ["https-443-tcp", "http-80-tcp"]
+
+#  ingress_with_cidr_blocks = [
+#    {
+#      from_port   = 8080
+#      to_port     = 8090
+#      protocol    = "tcp"
+#      description = "User-service ports"
+#      cidr_blocks = "10.0.0.0/16"
+#    },
+#    {
+#      rule        = "redis-tcp"
+#      cidr_blocks = "0.0.0.0/0"
+#    },
+#    {
+#      rule        = "smtps-465-tcp"
+#      cidr_blocks = "0.0.0.0/0"
+#    },
+#  ]
+
   tags = {
     Terraform = "true"
     Environment = var.envnonprod
