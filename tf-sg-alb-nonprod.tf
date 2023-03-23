2ecece02a9a0dd62b116edc6103e4f3e6e16fd47
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-23 14:10:38 +0200

    alb

---
+++ tf-sg-alb-nonprod.tf
@@ -0,0 +1,31 @@
+module "tf-sg-alb-nonprod" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "tf-sg-alb-nonprod"
+  vpc_id      = module.vpc-nonprod.vpc_id
+
+  ingress_cidr_blocks      = ["10.0.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.0.0.0/16"
+    },
+    {
+      rule        = "redis-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      rule        = "smtps-465-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+  tags = {
+    Terraform = "true"
+    Environment = var.envnonprod
+    Project = var.project
+  }
+}
