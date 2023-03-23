d9e44307a69390204824ef42ef5ba25236e31388
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-23 17:44:05 +0200

    s3 test

--- tf-s3.tf
+++ tf-s3.tf
@@ -17,3 +17,16 @@ module "s3-bucket-env" {
   }
 
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
