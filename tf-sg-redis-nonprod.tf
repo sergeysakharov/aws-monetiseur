8c3fccf45418dfc7d867d09d7b9e88015e38bb83
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-23 11:14:26 +0200

    redis update

--- tf-sg-redis-nonprod.tf
+++ tf-sg-redis-nonprod.tf
@@ -4,8 +4,8 @@ module "tf-sg-redis-nonprod" {
   name        = "tf-sg-redis-nonprod"
   vpc_id      = module.vpc-nonprod.vpc_id
 
-  ingress_cidr_blocks      = ["10.0.0.0/16"]
-  ingress_rules            = ["https-443-tcp"]
+#  ingress_cidr_blocks      = ["10.0.0.0/16"]
+#  ingress_rules            = ["https-443-tcp"]
   ingress_with_cidr_blocks = [
     {
       rule        = "redis-tcp"
