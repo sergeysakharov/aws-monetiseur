627703921265bae677375f298930d22074d6a371
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-23 11:12:09 +0200

    redis sg update

--- tf-sg-redis-nonprod.tf
+++ tf-sg-redis-nonprod.tf
@@ -8,19 +8,8 @@ module "tf-sg-redis-nonprod" {
   ingress_rules            = ["https-443-tcp"]
   ingress_with_cidr_blocks = [
     {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.0.0.0/16"
-    },
-    {
       rule        = "redis-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      rule        = "smtps-465-tcp"
-      cidr_blocks = "0.0.0.0/0"
+      cidr_blocks = "10.0.0.0/16"
     },
   ]
   tags = {
