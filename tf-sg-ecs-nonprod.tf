ba6658172612b8e68958ebdeb80ebf2c524970d6
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-23 23:18:21 +0200

    sg update

--- tf-sg-ecs-nonprod.tf
+++ tf-sg-ecs-nonprod.tf
@@ -4,6 +4,8 @@ module "tf-sg-ecs-nonprod" {
   name        = "tf-sg-ecs-nonprod"
   vpc_id      = module.vpc-nonprod.vpc_id
 
+  egress_cidr_blocks       = ["0.0.0.0/0"]
+  egress_rules             = [all-all]
   ingress_cidr_blocks      = ["10.0.0.0/16"]
   ingress_rules            = ["https-443-tcp"]
   ingress_with_cidr_blocks = [
