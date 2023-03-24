87fcf5bc9139c760e83f6147c16f81d36a094a50
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-24 15:58:49 +0200

    delete prod vpc

--- tf-vpc.tf
+++ tf-vpc.tf
@@ -20,24 +20,4 @@ module "vpc-nonprod" {
   }
 }
 
-module "vpc-prod" {
-  source = "terraform-aws-modules/vpc/aws"
 
-  name = "vpc-prod"
-  cidr = "10.10.0.0/16"
-
-  azs             = ["eu-west-3a", "eu-west-3b"]
-  private_subnets = ["10.10.1.0/24", "10.10.2.0/24"]
-  public_subnets  = ["10.10.101.0/24", "10.10.102.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = false
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  
-  tags = {
-    Terraform = "true"
-    Environment = var.envprod
-    Project = var.project
-  }
-}
