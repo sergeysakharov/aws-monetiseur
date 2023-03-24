6ba55d4dd1b7a4f10b6566598aafc63541e48ae3
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-24 15:08:22 +0200

    test

---
+++ vpc/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "eu-west-3"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+module "vpc-nonprod" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc-nonprod"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-3a", "eu-west-3b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  
+  tags = {
+    Terraform = "true"
+    Environment = var.envnonprod
+    Project = var.project
+  }
+}
