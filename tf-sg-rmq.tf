74985853d3042aeefae41cf4f4daa353ebed719b
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-24 15:52:09 +0200

    rmq update

--- tf-sg-rmq.tf
+++ tf-sg-rmq.tf
@@ -4,15 +4,23 @@ module "tf-sg-rmq-nonprod" {
   name        = "tf-sg-rmq-nonprod"
   vpc_id      = module.vpc-nonprod.vpc_id
 
-#  ingress_cidr_blocks      = ["10.0.0.0/16"]
-#  ingress_rules            = ["https-443-tcp"]
   egress_cidr_blocks       = ["0.0.0.0/0"]
   egress_rules             = ["all-all"]
+ 
   ingress_with_cidr_blocks = [
     {
       rule        = "rabbitmq-5672-tcp"
       cidr_blocks = "10.0.0.0/16"
     },
+    {
+      rule        = "rabbitmq-5672-tcp"
+      cidr_blocks = "15.188.231.122/32"
+    },
+    {
+      rule        = "rabbitmq-5672-tcp"
+      cidr_blocks = "35.181.70.198/32"
+    },
+
   ]
   tags = {
     Terraform = "true"
