f6dc13c76a63d2b78264af5d1287ab1a21dd0146
sergeysakharov <88827522+sergeysakharov@users.noreply.github.com>
2023-03-25 13:57:06 +0200

    full update

--- tf-rds.tf
+++ tf-rds.tf
@@ -51,27 +51,27 @@ resource "aws_db_parameter_group" "tf-rds-nonprod-param-group" {
   }
 }
 
-resource "aws_db_instance" "tf-rds-nonprod" {
-  identifier             = "tf-rds-nonprod"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 30
-  engine                 = "postgres"
-  engine_version         = "14.7"
-  username               = "psqladmin"
-  password               = var.pgsql-nonprod-password
-  db_subnet_group_name   = aws_db_subnet_group.tf-rds-nonprod-subnet-group.name
-  vpc_security_group_ids = [aws_security_group.tf-sg-rds-nonprod.id]
-  parameter_group_name   = aws_db_parameter_group.tf-rds-nonprod-param-group.name
-  publicly_accessible    = true
-  skip_final_snapshot    = true
+#resource "aws_db_instance" "tf-rds-nonprod" {
+#  identifier             = "tf-rds-nonprod"
+#  instance_class         = "db.t3.micro"
+#  allocated_storage      = 30
+#  engine                 = "postgres"
+#  engine_version         = "14.7"
+#  username               = "psqladmin"
+#  password               = var.pgsql-nonprod-password
+#  db_subnet_group_name   = aws_db_subnet_group.tf-rds-nonprod-subnet-group.name
+#  vpc_security_group_ids = [aws_security_group.tf-sg-rds-nonprod.id]
+#  parameter_group_name   = aws_db_parameter_group.tf-rds-nonprod-param-group.name
+#  publicly_accessible    = true
+#  skip_final_snapshot    = true
 
-  tags = {
-    Terraform = "true"
-    Environment = var.envnonprod
-    Project = var.project
-  }
+#  tags = {
+#    Terraform = "true"
+#    Environment = var.envnonprod
+#    Project = var.project
+#  }
 
-}
+#}
 
 resource "aws_db_instance" "tf-rds-dev" {
   identifier             = "tf-rds-dev"
